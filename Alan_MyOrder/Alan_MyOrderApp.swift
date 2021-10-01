72a9a676ddb07516dbf2a61df0df3e3c33f796ba
Alan Stagg <astagg@sheridancollege.ca>
2021-10-01 11:03:35 -0400

    Initial Commit

---
+++ Alan_MyOrder/Alan_MyOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Alan_MyOrderApp.swift
+//  Alan_MyOrder
+//
+//  Created by Alan Stagg on 2021-10-01.
+//
+
+import SwiftUI
+
+@main
+struct Alan_MyOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
